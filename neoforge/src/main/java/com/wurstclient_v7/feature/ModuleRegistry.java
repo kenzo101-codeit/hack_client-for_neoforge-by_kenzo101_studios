db60cf7a86baf49c7c57272815e5ff22daef9092
kenzo101-codeit <kenzo101studios@gmail.com>
2026-01-07 10:01:45 -0300

    ...

--- neoforge/src/main/java/com/wurstclient_v7/feature/ModuleRegistry.java
+++ neoforge/src/main/java/com/wurstclient_v7/feature/ModuleRegistry.java
@@ -7,27 +7,27 @@ public final class ModuleRegistry {
 	public static final Map<String, ModuleToggle> MODULES = new LinkedHashMap<>();
 
 	static {
-		MODULES.put("KillAura", KillAura::isEnabled);
-		MODULES.put("AutoAttack", AutoAttack::isEnabled);
-		MODULES.put("SpeedHack", SpeedHack::isEnabled);
-		MODULES.put("FullBright", FullBright::isEnabled);
-		MODULES.put("Flight", Flight::isEnabled);
-		MODULES.put("NoFall", NoFall::isEnabled);
-		MODULES.put("XRay", XRay::isEnabled);
-		MODULES.put("Jetpack", Jetpack::isEnabled);
-		MODULES.put("Nuker", Nuker::isEnabled);
-		MODULES.put("Spider", Spider::isEnabled);
-		MODULES.put("ESP", ESP::isEnabled);
-		MODULES.put("Tracers", Tracers::isEnabled);
-		MODULES.put("AndromedaBridge", AndromedaBridge::isEnabled);
-		MODULES.put("SafeWalk", SafeWalk::isEnabled);
-		MODULES.put("GodMode", GodMode::isEnabled);
-		MODULES.put("Freecam", Freecam::isEnabled);
-		MODULES.put("LSD", LSD::isEnabled);
-		MODULES.put("Jesus", Jesus::isEnabled);
-		MODULES.put("Glide", Glide::isEnabled);
-		MODULES.put("AirPlace", AirPlace::isEnabled);
-		MODULES.put("BoatFly", BoatFly::isEnabled);
+		MODULES.put("KillAura", KillAura.isEnabled);
+		MODULES.put("AutoAttack", AutoAttack.isEnabled);
+		MODULES.put("SpeedHack", SpeedHack.isEnabled);
+		MODULES.put("FullBright", FullBright.isEnabled);
+		MODULES.put("Flight", Flight.isEnabled);
+		MODULES.put("NoFall", NoFall.isEnabled);
+		MODULES.put("XRay", XRay.isEnabled);
+		MODULES.put("Jetpack", Jetpack.isEnabled);
+		MODULES.put("Nuker", Nuker.isEnabled);
+		MODULES.put("Spider", Spider.isEnabled);
+		MODULES.put("ESP", ESP.isEnabled);
+		MODULES.put("Tracers", Tracers.isEnabled);
+		MODULES.put("AndromedaBridge", AndromedaBridge.isEnabled);
+		MODULES.put("SafeWalk", SafeWalk.isEnabled);
+		MODULES.put("GodMode", GodMode.isEnabled);
+		MODULES.put("Freecam", Freecam.isEnabled);
+		MODULES.put("LSD", LSD.isEnabled);
+		MODULES.put("Jesus", Jesus.isEnabled);
+		MODULES.put("Glide", Glide.isEnabled);
+		MODULES.put("AirPlace", AirPlace.isEnabled);
+		MODULES.put("BoatFly", BoatFly.isEnabled);
 	}
 
 	public interface ModuleToggle {
